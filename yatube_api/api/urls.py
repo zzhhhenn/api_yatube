d30dcdb6b72aab8d6585ca72037afc6d69b08184
zzhhhenn <zhekabandukova@gmail.com>
2026-09-15 17:50:51 +0300

    Fix comment routes

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -9,7 +9,7 @@ router = DefaultRouter()
 router.register('posts', PostViewSet, basename='posts')
 router.register('groups', GroupViewSet, basename='groups')
 router.register(
-    r'posts/(?P<post_id>d+)/comments',
+    r'posts/(?P<post_id>\d+)/comments',
     CommentViewSet,
     basename='comments',
 )
